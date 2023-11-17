afb52d49aa22eac3fd08a60bc27688917b070441
keroru24990 <a24990@svalero.com>
2023-11-17 20:35:05 +0100

    032

--- Vinted/app/src/main/java/com/example/vinted/lstProducts/model/ModelListProducts.java
+++ Vinted/app/src/main/java/com/example/vinted/lstProducts/model/ModelListProducts.java
@@ -1,13 +1,16 @@
 package com.example.vinted.lstProducts.model;
 
+import com.example.vinted.conn.Connector;
 import com.example.vinted.lstProducts.ContractListProducts;
 import com.example.vinted.lstProducts.bean.Producto;
 
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 public class ModelListProducts implements ContractListProducts.Model {
 
 
+    Connector conn = new Connector();
 
     public ArrayList<Producto> listProducts(){
         return null;
@@ -15,7 +18,8 @@ public class ModelListProducts implements ContractListProducts.Model {
 
 
     @Override
-    public void productsAPI(String filtro, OnProductsListener respuestaMovies) {
-
+    public ResultSet productsAPI(String filtro, OnProductsListener respuestaMovies) {
+        String SQL = "";
+        return conn.executeQuery(SQL);
     }
 }
