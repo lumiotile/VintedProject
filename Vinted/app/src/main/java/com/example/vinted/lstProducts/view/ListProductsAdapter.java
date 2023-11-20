2a0d58397f6cf1d0f3e6c7f6707f60dfe167db8d
keroru24990 <a24990@svalero.com>
2023-11-20 22:06:06 +0100

    25

---
+++ Vinted/app/src/main/java/com/example/vinted/lstProducts/view/ListProductsAdapter.java
@@ -0,0 +1,52 @@
+package com.example.vinted.lstProducts.view;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.vinted.R;
+import com.example.vinted.lstProducts.bean.Producto;
+
+import java.util.ArrayList;
+
+public class ListProductsAdapter extends RecyclerView.Adapter<ListProductsAdapter.ContactViewHolder> {
+
+    ArrayList<Producto> listaProductos;
+
+    public ListProductsAdapter(ArrayList<Producto> listaProductos){
+        this.listaProductos = listaProductos;
+    }
+    @NonNull
+    @Override
+    public ListProductsAdapter.ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_product_list, null, false);
+        return new ContactViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListProductsAdapter.ContactViewHolder holder, int position) {
+        holder.viewNombre.setText(listaProductos.get(position).getNombre());
+        holder.viewPrecio.setText(String.valueOf(listaProductos.get(position).getPrecio()));
+        holder.viewDescripcion.setText(listaProductos.get(position).getDescripcion());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaProductos.size();
+    }
+
+    public class ContactViewHolder extends RecyclerView.ViewHolder {
+        TextView viewNombre, viewDescripcion, viewPrecio;
+        public ContactViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            viewNombre = itemView.findViewById(R.id.viewNombre);
+            viewDescripcion = itemView.findViewById(R.id.viewDescripcion);
+            viewPrecio = itemView.findViewById(R.id.viewPrecio);
+        }
+    }
+}
--- Vinted/app/src/main/java/com/example/vinted/lstProducts/view/ProductListActivity.java
+++ Vinted/app/src/main/java/com/example/vinted/lstProducts/view/ProductListActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -16,16 +17,20 @@ import java.util.ArrayList;
 public class ProductListActivity extends AppCompatActivity implements ContractListProducts.View {
 
     RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
+        recyclerView = findViewById(R.id.recyclerView);
         Toast.makeText(getApplicationContext(), "Login correcto", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
     public void successProducts(ArrayList<Producto> lstProducts) {
-        
+        ListProductsAdapter adapter = new ListProductsAdapter(lstProducts);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
